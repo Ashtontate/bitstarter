3c2de3450ad2eb92344700854e5d76d5a2e3e136
Ashtontate <vladbes@gmail.com>
2013-07-12 09:21:56 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -1,14 +1,15 @@
+
 var express = require('express');
 var fs = require('fs');
-var buf = new Buffer('test');
+var buf = new Buffer(fs.readFileSync('index.html'));
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(buf.toString(fs.readFileSync('index.html')));
+    response.send(buf.toString('ascii'));
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
-  console.log("Listening on " + port);
+    console.log("Listening on " + port);
 });
